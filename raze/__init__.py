70a0f3b5f3ae1183b470d816ddca3af43e6a257b
Devil1million <ObitoAppnaBhaiHai@gmail.com>
2026-05-11 12:14:58 +0530

    fix: plugins path

--- raze/__init__.py
+++ raze/__init__.py
@@ -33,7 +33,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins={"root": "raze/modules"},
+    plugins={"root": "raze.modules"},
 )
 from dotenv import load_dotenv
 load_dotenv()
